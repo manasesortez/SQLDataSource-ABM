1580517497d5d1789ec6e2f9255c3cb6d5d3ddf6
manasesortez <manasesortez1234@hotmail.com>
2022-03-29 14:37:20 -0600

    SQLDataSource

--- Clase-17ABM/Modificaciones.aspx.cs
+++ Clase-17ABM/Modificaciones.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Clase_17ABM
 {
-    public partial class Default : System.Web.UI.Page
+    public partial class Modificaciones : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
